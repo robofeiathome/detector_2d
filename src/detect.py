7b848ba0129a287fa6e9f5f93b304c9b2e8262df
Cauan Sousa <103227067+cauansousa@users.noreply.github.com>
2023-03-01 09:32:50 -0300

    Otimizando detect.py

--- src/detect.py
+++ src/detect.py
@@ -1,32 +1,23 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 
 import rospy
 import numpy as np
 import tf
 
-from calendar import c
-from pyexpat import model
-
-from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs import point_cloud2 as pc2
-from sensor_msgs.msg import Image, PointCloud2
-import rospy
-from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image, PointCloud2
 from hera_face.srv import face_list
-import numpy as np
 import torch
 from ultralytics import YOLO
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 
 import processing as pr
-#from PIL import Image
 
 class Detector:
 
-    def __init__(self):
+    def _init_(self):
         image_topic = '~/zed2/zed_node/left_raw/image_raw_color'
         point_cloud_topic = '/zed2/zed_node/point_cloud/cloud_registered'
 
@@ -38,10 +29,10 @@ class Detector:
 
         # image and point cloud subscribers
         # and variables that will hold their values
-        rospy.Subscriber(image_topic, Image, self.image_callback)
+        self._image_sub = rospy.Subscriber(image_topic, Image, self.image_callback)
 
         if point_cloud_topic is not None:
-            rospy.Subscriber(point_cloud_topic, PointCloud2, self.pc_callback)
+            self._pc_sub = rospy.Subscriber(point_cloud_topic, PointCloud2, self.pc_callback)
         else:
             rospy.loginfo(
                 'No point cloud information available. Objects will not be placed in the scene.')
@@ -60,36 +51,30 @@ class Detector:
         # Store value on a private attribute
         self._current_image = image
 
-    def pc_callback(self, pc):
-        """Point cloud callback"""
-        # Store value on a private attribute
-        self._current_pc = pc
-
     def run(self):
         # run while ROS runs
         while not rospy.is_shutdown():
             # only run if there's an image present
             if self._current_image is not None:
                 try:
-                    #Busca imagem
+                    #Search image
                     small_frame = self._bridge.imgmsg_to_cv2(self._current_image, desired_encoding='bgr8')
                     
-                    #Carrega o modelo
+                    #Load model
                     yolo = YOLO("yolov8n.pt")
                     results = yolo.predict(source=small_frame, conf=0.8)
                     boxes = results[0].boxes
                     
-                    #Coloca as bboxes
+                    #Plot bbox 
                     small_frame = pr.plot_bboxes(small_frame, boxes.boxes, conf=0.5)
-                    #small_frame = results[0].plot() <-- outra opção de colocar bbox
-
-                    #publica o topico
+                    
+                    #Publisher
                     self._imagepub.publish(self._bridge.cv2_to_imgmsg(small_frame, 'rgb8'))
 
                 except CvBridgeError as e:
                     print(e)
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     rospy.init_node('detector_2d', log_level=rospy.INFO)
 
     try:
